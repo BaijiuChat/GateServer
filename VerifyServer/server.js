6cc32f8a96afd2d139166b7aa35c643c1d7659c0
Riustina <Riustina@outlook.com>
2025-04-16 19:22:07 +0800

    透过Redis设置验证码过期

--- VerifyServer/server.js
+++ VerifyServer/server.js
@@ -1,25 +1,37 @@
 const grpc = require('@grpc/grpc-js')
+const crypto = require('crypto');
 const message_proto = require('./proto')
 const const_module = require('./const')
-const { v4:uuidv4 } = require('uuid')
+const redis_module = require('./redis')
 const emailModule = require ('./email')
+
+function generateSixDigitCode() {
+    return crypto.randomInt(0, 999999).toString().padStart(6, '0');
+}
 
 // call是请求，callback是回调函数，注意async
 async function GetVerifyCode(call, callback) {
-    console.log("email is ", call.request.email)
-    console.log("GetVerifyCode is called")
+    console.log("GetVerifyCode is called, email is ", call.request.email)
 
-    function generateSixDigitCode() {
-        const randomNum = Math.floor(Math.random() * 1000000); // 0~999999
-        return randomNum.toString().padStart(6, '0'); // 不足6位补零
-    }
+    let query_res = await redis_module.QueryRedis(const_module.code_prefix+call.request.email);
+    console.log("query res is ", query_res)
 
     //生成验证码
-    let uniqueId = null;
     try{
-        uniqueId = generateSixDigitCode();
+        const uniqueId = generateSixDigitCode();
+        const redisKey = const_module.code_prefix+call.request.email;
+        // 只是为了方便测试，过期时间设置为了15秒
+        const bres = await redis_module.SetRedisExpire(redisKey, uniqueId, 15);
+        if(!bres){
+            console.log("set redis error is ", bres)
+            callback(null, { 
+                email: call.request.email,
+                error: const_module.Errors.Exception
+            }); 
+            return
+        }
         console.log(" 验证码是 ", uniqueId)
-        let text_str = `您的验证码如下:\n\n<b><font size="6">${uniqueId}</font></b>\n\n使用该验证码验证您的邮箱并完成你的注册。\n\n如果您并没有请求验证码，请无视该邮件。\n\n白久无瑕团队敬上`;
+        let text_str = `您的验证码如下:\n<b><font size="6">${uniqueId}</font></b>\n使用该验证码验证您的邮箱并完成你的注册。\n验证码有效期为5分钟。\n如果您并没有请求验证码，请无视该邮件。\n\n白久无瑕团队敬上`;
         //发送邮件
         let mailOptions = {
             from: 'baijiuwuhu@163.com',
@@ -33,16 +45,16 @@ async function GetVerifyCode(call, callback) {
         let send_res = await emailModule.SendMail(mailOptions);
         console.log("send res is ", send_res)
 
-        callback(null, { email:  call.request.email,
-            error:const_module.Errors.Success
+        callback(null, { 
+            email: call.request.email,
+            error: const_module.Errors.Success
         }); 
-
-
     }catch(error){
-        console.log("catch error is ", error)
+        console.log("Error in GetVerifyCode: ", error)
         // 这里的null指的是有没有发生底层的错误
-        callback(null, { email:  call.request.email,
-            error:const_module.Errors.Exception
+        callback(null, { 
+            email: call.request.email,
+            error: const_module.Errors.Exception
         }); 
     }
 
@@ -51,10 +63,15 @@ async function GetVerifyCode(call, callback) {
 function main() {
     var server = new grpc.Server()
     server.addService(message_proto.VerifyService.service, { GetVerifyCode: GetVerifyCode })
-    server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), () => {
+    server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), (err, port) => {
         // server.start() 已经不需要，bindAsync会自动调用bindAsync
-        console.log('gRPC 服务已启动，监听端口 50051')        
-    })
+        if (err) {
+            console.error("Server bind failed:", err);
+            process.exit(1);
+          }
+          // server.start();
+          console.log('gRPC server running on port', port);
+        });
 }
 
 main()
