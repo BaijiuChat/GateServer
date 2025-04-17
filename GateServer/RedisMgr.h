92aaccbac4ef99445bf9647959a18cdc54951bd2
Riustina <Riustina@outlook.com>
2025-04-17 13:02:34 +0800

    初步封装redis连接池
    尚未完成初始化

--- GateServer/RedisMgr.h
+++ GateServer/RedisMgr.h
@@ -1,6 +1,27 @@
 #pragma once
+#include <atomic>
+#include <queue>
 #include "Singleton.h"
 #include "hiredis/hiredis.h"
+#include "ConfigMgr.h"
+
+class RedisConPool {
+public:
+    RedisConPool(size_t poolSize, const char* host, int port, const char* pwd);
+    ~RedisConPool();
+    redisContext* getConnection();
+    void returnConnection(redisContext* context);
+    void Close();
+
+private:
+    std::atomic<bool> b_stop_;
+    size_t poolSize_;
+    const char* host_;
+    int port_;
+    std::queue<redisContext*> connections_;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+};
 
 class RedisMgr : public Singleton<RedisMgr>
 {
