38808392da7b59efea373a45c9af87df32c9b73f
Riustina <Riustina@outlook.com>
2025-04-17 12:13:44 +0800

    封装redis操作类

--- GateServer/GateServer.cpp
+++ GateServer/GateServer.cpp
@@ -3,9 +3,35 @@
 #include <iostream>
 #include <cstdlib>      // 用于 EXIT_FAILURE
 #include <boost/asio/signal_set.hpp>
+#include <cassert>
+#include "RedisMgr.h"
+
+// void TestRedisMgr() {
+//	assert(RedisMgr::GetInstance()->Connect("127.0.0.1", 6379));
+//	assert(RedisMgr::GetInstance()->Auth("root"));
+//	assert(RedisMgr::GetInstance()->Set("blogwebsite", "llfc.club"));
+//	std::string value = "";
+//	assert(RedisMgr::GetInstance()->Get("blogwebsite", value));
+//	assert(RedisMgr::GetInstance()->Get("nonekey", value) == false);
+//	assert(RedisMgr::GetInstance()->HSet("bloginfo", "blogwebsite", "llfc.club"));
+//	assert(RedisMgr::GetInstance()->HGet("bloginfo", "blogwebsite") != "");
+//	assert(RedisMgr::GetInstance()->ExistsKey("bloginfo"));
+//	assert(RedisMgr::GetInstance()->Del("bloginfo"));
+//	assert(RedisMgr::GetInstance()->Del("bloginfo"));
+//	assert(RedisMgr::GetInstance()->ExistsKey("bloginfo") == false);
+//	assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue1"));
+//	assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue2"));
+//	assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue3"));
+//	assert(RedisMgr::GetInstance()->RPop("lpushkey1", value));
+//	assert(RedisMgr::GetInstance()->RPop("lpushkey1", value));
+//	assert(RedisMgr::GetInstance()->LPop("lpushkey1", value));
+//	assert(RedisMgr::GetInstance()->LPop("lpushkey2", value) == false);
+//	RedisMgr::GetInstance()->Close();
+//}
 
 int main()
 {
+	// TestRedisMgr(); // 测试RedisMgr
 	auto& g_config_mgr = ConfigMgr::Inst(); // 创建ConfigMgr实例
 	std::string gate_port_str = g_config_mgr["GateServer"]["port"];
 	unsigned short gate_port = atoi(gate_port_str.c_str());
--- GateServer/LogicSystem.cpp
+++ GateServer/LogicSystem.cpp
@@ -5,6 +5,7 @@
 #include <boost/beast/core/ostream.hpp>
 #include <json/json.h>
 #include <json/reader.h>
+//#include "RedisMgr.h"
 
 void LogicSystem::RegGet(std::string url, HttpHandler handler)
 {
@@ -55,6 +56,63 @@ LogicSystem::LogicSystem()
 		beast::ostream(connection->_response.body()) << jsonstr;
 		return true;
 		});
+
+	//RegPost("/user_register", [](std::shared_ptr<HttpConnection> connection) {
+	//	auto body_str = boost::beast::buffers_to_string(connection->_request.body().data());
+	//	std::cout << "receive body is " << body_str << std::endl;
+	//	connection->_response.set(http::field::content_type, "text/json");
+	//	Json::Value root;
+	//	Json::Reader reader;
+	//	Json::Value src_root;
+	//	bool parse_success = reader.parse(body_str, src_root);
+	//	if (!parse_success) {
+	//		std::cout << "Failed to parse JSON data!" << std::endl;
+	//		root["error"] = ErrorCodes::Error_Json;
+	//		std::string jsonstr = root.toStyledString();
+	//		beast::ostream(connection->_response.body()) << jsonstr;
+	//		return true;
+	//	}
+	//	//先查找redis中email对应的验证码是否合理
+	//	std::string  verify_code;
+	//	bool b_get_verify = RedisMgr::GetInstance()->Get(src_root["email"].asString(), verify_code);
+	//	if (!b_get_verify) {
+	//		std::cout << " get verify code expired" << std::endl;
+	//		root["error"] = ErrorCodes::verifyExpired;
+	//		std::string jsonstr = root.toStyledString();
+	//		beast::ostream(connection->_response.body()) << jsonstr;
+	//		return true;
+	//	}
+
+	//	if (verify_code != src_root["verifycode"].asString()) {
+	//		std::cout << " verify code error" << std::endl;
+	//		root["error"] = ErrorCodes::verifyCodeErr;
+	//		std::string jsonstr = root.toStyledString();
+	//		beast::ostream(connection->_response.body()) << jsonstr;
+	//		return true;
+	//	}
+
+	//	//访问redis查找
+	//	bool b_usr_exist = RedisMgr::GetInstance()->ExistsKey(src_root["user"].asString());
+	//	if (b_usr_exist) {
+	//		std::cout << " user exist" << std::endl;
+	//		root["error"] = ErrorCodes::UserExist;
+	//		std::string jsonstr = root.toStyledString();
+	//		beast::ostream(connection->_response.body()) << jsonstr;
+	//		return true;
+	//	}
+
+	//	//查找数据库判断用户是否存在
+
+	//	root["error"] = 0;
+	//	root["email"] = src_root["email"];
+	//	root["user"] = src_root["user"].asString();
+	//	root["passwd"] = src_root["passwd"].asString();
+	//	root["confirm"] = src_root["confirm"].asString();
+	//	root["verifycode"] = src_root["verifycode"].asString();
+	//	std::string jsonstr = root.toStyledString();
+	//	beast::ostream(connection->_response.body()) << jsonstr;
+	//	return true;
+	//	});
 }
 
 bool LogicSystem::HandleGet(std::string path, std::shared_ptr<HttpConnection> con)
